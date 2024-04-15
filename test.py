c0f6ea235b974ae9979a127b6c7218eb82e78533
quanth02 <anhquankaka113@gmail.com>
2024-04-15 21:40:02 +0700

    crawl-the-numbers

--- test.py
+++
@@ -1,7 +0,0 @@
-text = "Eiga Doraemon: Nobita to Sora no Utopia (映画ドラえもん のび太と空の理想郷（ユートピア）) (2023)"
-# Tách chuỗi bằng dấu ngoặc đơn '(' và lấy phần tử cuối cùng
-year = text.split('(')[-1].strip(')')
-
-a = text.split(' (')[0]
-
-print(a)
