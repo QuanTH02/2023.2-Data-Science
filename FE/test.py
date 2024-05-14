cd2304c9ac255792e797ea373568a66399b6e8c9
quanth02 <anhquankaka113@gmail.com>
2024-05-14 12:11:55 +0700

    update

--- FE/test.py
+++ FE/test.py
@@ -1,7 +1,16 @@
-str1 = '"Roman J. Israel, Esq.",1.0,2018.0,,,,'
+import pandas as pd
 
-a = str1.split("\"")
+df = pd.read_csv("../critic_metascore/data/all_data_critic_metascore.csv")
+movie_name_list = df["movie_name"].tolist()
 
-b = a[2].split(",")
+critic_vote_metacritic_list = df["critic_vote_metacritic"].tolist()
+meta_score_metacritic_list = df["meta_score_metacritic"].tolist()
+critic_vote_rotten_list = df["critic_vote_rotten"].tolist()
+meta_score_rotten_list = df["meta_score_rotten"].tolist()
 
-print(b[1])
+index = 0
+for movie_name in movie_name_list:
+    if pd.isnull(critic_vote_metacritic_list[movie_name_list.index(movie_name)]) and pd.isnull(critic_vote_rotten_list[movie_name_list.index(movie_name)]):
+        index += 1
+
+print(index)
