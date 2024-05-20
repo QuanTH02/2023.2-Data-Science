c6c933e76be79e5146f63f0c5b35d7d203b48d52
quanth02 <anhquankaka113@gmail.com>
2024-05-20 08:54:39 +0700

    crawl_update

--- main/main_update.py
+++ main/main_update.py
@@ -5,7 +5,7 @@ from crawl_update_critic_metascore import *
 from crawl_update_themoviedb import *
 
 if __name__ == '__main__':
-    # main_mojo()
+    main_mojo()
     main_tmdb()
     main_imdb()
     main_critic_metascore()
